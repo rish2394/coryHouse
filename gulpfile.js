449ba996033e6b16d7a4f68d11797de8e625c263
Rishabh <rishabh.gupta998@gmail.com>
2016-08-15 22:24:21 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,37 @@
- 
+"use strict"
+
+var gulp=require('gulp');
+var connect=require('gulp-connect');
+var open=require('gulp-open');
+
+var config={
+          port:9000,
+          devBaseURL: 'http://localhost',
+          paths: {
+              html: './src/*.html',
+              dist: './dist'
+          }
+}
+//Start a local development server
+gulp.task('connect',function(){
+  connect.server({
+          root: ['dist'],
+          port: config.port,
+          base: config.devBaseURL,
+          livereload: true
+  });
+});
+
+gulp.task('open',['connect'],function(){
+     gulp.src('dist/index.html').pipe(open('',{url:config.devBaseURL + ':' + config.port + '/'}));
+});
+
+gulp.task('html',function(){
+     gulp.src(config.paths.html).pipe(gulp.dest(config.paths.dist)).pipe(connect.reload());
+})
+
+gulp.task('watch',function(){
+     gulp.watch(config.path.html,['html']);
+})
+
+gulp.task('default',['html','open','watch'])

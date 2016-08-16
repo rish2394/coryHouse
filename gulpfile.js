754334c4249c3f3c2b2f3f368466613c1db0b18d
Rishabh <rishabh.gupta998@gmail.com>
2016-08-16 23:44:06 +0530

    Add css,eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 "use strict"
 
 var gulp=require('gulp');
-var connect=require('gulp-connect');
-var open=require('gulp-open');
+var connect=require('gulp-connect');//Runs a local dev server
+var open=require('gulp-open'); //Open a URL in a web browser
+var browserify=require('browserify');//Bundles JS
+var reactify=require('reactify');//Transform React JSX into JS
+var source=require('vinyl-source-stream');//Use conventional text stream with GUlp
+var concat=require('gulp-concat');//Concatenates file
+var lint=require('gulp-eslint');//Lint JS files,including JSX
 
 var config={
           port:9000,
           devBaseURL: 'http://localhost',
           paths: {
               html: './src/*.html',
-              dist: './dist'
+              js: './src/**/*.js',
+              css: [
+                   'node_modules/bootstrap/dist/css/bootstrap.min.css',
+                   'node_modules/bootstrap/dist/css/bootstrap.theme.min.css'
+              ],
+              dist: './dist',
+              mainJS: './src/main.js'
           }
 }
 //Start a local development server
@@ -30,8 +41,23 @@ gulp.task('html',function(){
      gulp.src(config.paths.html).pipe(gulp.dest(config.paths.dist)).pipe(connect.reload());
 })
 
+gulp.task('js',function(){
+     browserify(config.paths.mainJS).transform(reactify).bundle()
+     .on('error',console.error.bind(console)).pipe(source('bundle.js'))
+     .pipe(gulp.dest(config.paths.dist + '/scripts')).pipe(connect.reload());
+});
+
+gulp.task('css',function(){
+     gulp.src(config.paths.css).pipe(concat('bundle.css')).pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint',function(){
+     return gulp.src(config.paths.js).pipe(lint({config: 'eslint.config.json'})).pipe(lint.format());
+});
+
 gulp.task('watch',function(){
-     gulp.watch(config.path.html,['html']);
+     gulp.watch(config.paths.html,['html']);
+     gulp.watch(config.paths.js,['js','lint']);
 })
 
-gulp.task('default',['html','open','watch'])
+gulp.task('default',['html','js','css','open','watch','lint']);
